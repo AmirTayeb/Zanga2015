e3f62fe0b9aa7175ca2eed51a4f227896a58ec81
Muhammad <Ezioo1992@gmail.com>
2015-08-02 01:43:57 +0200

    Added Main Menu scene, organized hierarchy in level 1

---
+++ RightTime/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour 
+{
+    public GameObject creditsPanel;
+    public GameObject MainMenuPanel;
+
+    public void NewGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+        MainMenuPanel.SetActive(false);  
+    }
+    
+    public void Back()
+    {
+        creditsPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
