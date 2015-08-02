d2f6df33f5d440383e801b2f5d30383e74463742
Muhammad <Ezioo1992@gmail.com>
2015-08-02 21:30:38 +0200

    Intro Scene

---
+++ RightTime/Assets/Scripts/Intro.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour
+{
+    public Image intro;
+    public Image story;
+    public Text escText;
+
+    public float delayTime = 1.5f;
+
+    private bool imageSwitched = false;
+    void Update()
+    {
+        StartCoroutine(EscTextShowUp());
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (imageSwitched)
+            {
+                Application.LoadLevel(2);
+            }
+            else
+            {
+                ImageSwitch();
+            }
+        }
+            
+        
+    }
+
+    void ImageSwitch()
+    {
+        intro.sprite = story.sprite;
+        imageSwitched = true;
+    }
+
+    IEnumerator EscTextShowUp()
+    {
+        yield return new WaitForSeconds(delayTime);
+        escText.enabled = true;
+    }
+}
