83aacf45568e3a5d38b19f7d8d90513fb528780a
AmirTayeb <san_moon101@hotmail.com>
2015-08-03 00:33:54 +0300

    أخر التعديلات على كود المحادثة

---
+++ RightTime/Assets/Scripts/Backtomenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Backtomenu : MonoBehaviour {
+
+	public void BackToMainMenu()
+	{
+		Application.LoadLevel(0);
+	}
+	
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
--- RightTime/Assets/Scripts/talk_control.cs
+++ RightTime/Assets/Scripts/talk_control.cs
@@ -30,8 +30,11 @@ public class talk_control : MonoBehaviour {
 	public Transform End_4;
 	public Transform End_5;
 	public Transform info;
-	
+	public Transform press;
 	public bool add_count; 
+	public Transform bck;
+	public Transform exit;
+
 	public int count =0 ;
 	// Use this for initialization
 	void Start () 
@@ -45,29 +48,68 @@ public class talk_control : MonoBehaviour {
 	{
 		if (count == 0) 
 		{
-			P_start.gameObject.SetActive(true);
-			F_start.gameObject.SetActive(true);
-			add_count = true;	
+			StartCoroutine(delayedTime());
+			add_count = true;
 		}
 		
 		if (Input.GetKeyUp (KeyCode.Space) && count == 1) 
 		{
+			press.gameObject.SetActive(false);
+			P_start.gameObject.SetActive(false);
+			F_start.gameObject.SetActive(false);
+			P_empty.gameObject.SetActive(true);
+			P_carefull.gameObject.SetActive(true);
+			P_2_1.gameObject.SetActive(true);
+			P_2_2.gameObject.SetActive(true);
+			P_2_3.gameObject.SetActive(true);
+			P_2_4.gameObject.SetActive(true);
 
-			
 			add_count = true;
 		}
 		
 		if (Input.GetKeyUp (KeyCode.Space) && count == 2) 
 		{
-			
+			F_2_2.gameObject.SetActive(false);
+			press.gameObject.SetActive(false);
+			P_3.gameObject.SetActive(true);
+			F_3.gameObject.SetActive(true);
 
 			add_count = true;
 			
 			
 		}
 		
-		
-	
+		if (Input.GetKeyUp (KeyCode.Space) && count == 3) 
+		{
+			F_2_2.gameObject.SetActive(false);
+			press.gameObject.SetActive(false);
+			P_3.gameObject.SetActive(true);
+			F_3.gameObject.SetActive(true);
+			
+			add_count = true;
+			
+			
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space) && count == 4) 
+		{
+			F_3.gameObject.SetActive(false);
+			P_3.gameObject.SetActive(false);
+			press.gameObject.SetActive(false);
+			P_4_1.gameObject.SetActive(true);
+			P_4_2.gameObject.SetActive(true);
+			P_empty.gameObject.SetActive(true);
+			P_carefull.gameObject.SetActive(true);
+			
+			add_count = true;
+			
+			
+		}
+
+		if (count == 3) 
+		{
+			press.gameObject.SetActive(true);
+		}
 		
 
 		
@@ -76,5 +118,111 @@ public class talk_control : MonoBehaviour {
 			count++;	
 			add_count = false;
 		}
+	}
+
+	IEnumerator delayedTime()
+	{
+		P_start.gameObject.SetActive(true);
+		yield return new WaitForSeconds(1f);
+		F_start.gameObject.SetActive(true);
+		press.gameObject.SetActive(true);
+	}
+
+	public void first_pick()
+	{
+		P_empty.gameObject.SetActive(false);
+		P_carefull.gameObject.SetActive(false);
+		P_2_1.gameObject.SetActive(false);
+		P_2_2.gameObject.SetActive(false);
+		P_2_3.gameObject.SetActive(false);
+		P_2_4.gameObject.SetActive(false);
+		F_2_1.gameObject.SetActive(true);
+		End_1.gameObject.SetActive (true);
+		bck.gameObject.SetActive (true);
+		exit.gameObject.SetActive (true);
+		friend_animation_control.player_fail = true;
+
+	}
+
+	public void second_pick()
+	{
+		P_empty.gameObject.SetActive(false);
+		P_carefull.gameObject.SetActive(false);
+		P_2_1.gameObject.SetActive(false);
+		P_2_2.gameObject.SetActive(false);
+		P_2_3.gameObject.SetActive(false);
+		P_2_4.gameObject.SetActive(false);
+		F_2_2.gameObject.SetActive(true);
+		press.gameObject.SetActive(true);
+
+	}
+
+	public void third_pick()
+	{
+		P_empty.gameObject.SetActive(false);
+		P_carefull.gameObject.SetActive(false);
+		P_2_1.gameObject.SetActive(false);
+		P_2_2.gameObject.SetActive(false);
+		P_2_3.gameObject.SetActive(false);
+		P_2_4.gameObject.SetActive(false);
+		F_2_3.gameObject.SetActive(true);
+		End_3.gameObject.SetActive (true);
+		bck.gameObject.SetActive (true);
+		exit.gameObject.SetActive (true);
+		friend_animation_control.player_fail = true;
+		
+	}
+
+	public void forth_pick()
+	{
+		P_empty.gameObject.SetActive(false);
+		P_carefull.gameObject.SetActive(false);
+		P_2_1.gameObject.SetActive(false);
+		P_2_2.gameObject.SetActive(false);
+		P_2_3.gameObject.SetActive(false);
+		P_2_4.gameObject.SetActive(false);
+		F_2_4.gameObject.SetActive(true);
+		End_4.gameObject.SetActive (true);
+		bck.gameObject.SetActive (true);
+		exit.gameObject.SetActive (true);
+		friend_animation_control.player_fail = true;
+		
+	}
+
+	public void forth1_pick()
+	{
+		P_4_1.gameObject.SetActive(false);
+		P_4_2.gameObject.SetActive(false);
+		P_empty.gameObject.SetActive(false);
+		P_carefull.gameObject.SetActive(false);
+		End_2.gameObject.SetActive (true);
+		F_4_1.gameObject.SetActive(true);
+		bck.gameObject.SetActive (true);
+		exit.gameObject.SetActive (true);
+		friend_animation_control.player_fail = true;
+		
+	}
+
+	public void forth2_pick()
+	{
+		P_4_1.gameObject.SetActive(false);
+		P_4_2.gameObject.SetActive(false);
+		P_empty.gameObject.SetActive(false);
+		P_carefull.gameObject.SetActive(false);
+		F_4_2.gameObject.SetActive(true);
+		End_5.gameObject.SetActive (true);
+		bck.gameObject.SetActive (true);
+		exit.gameObject.SetActive (true);
+
+	}
+
+	public void BackToMainMenu()
+	{
+		Application.LoadLevel(0);
+	}
+	
+	public void ExitGame()
+	{
+		Application.Quit();
 	}
 }
